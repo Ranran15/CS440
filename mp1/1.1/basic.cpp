446cdab9045681f5b85e4ad67d60dc6b1785aa06
Ranran15 <rrui1510@gmail.com>
2015-09-26 06:02:56 -0500

    without Graph

---
+++ mp1/1.1/basic.cpp
@@ -0,0 +1,46 @@
+#include "BasicPath.h"
+
+#include <queue>
+
+using namespace std;
+
+
+int DFS(int ** maze, int startx, int starty, int endx, int endy){
+
+}
+
+int BFS(int ** maze, int startx, int starty, int endx, int endy) {
+
+}
+
+int Greedy(int ** maze, int startx, int starty, int endx, int endy) {
+
+}
+
+
+int AStar(int ** maze, int startx, int starty, int endx, int endy) {
+
+}
+
+int** maze_reader(char* filename) {
+	i//nt ** a = 
+}
+
+void maze_printer(int ** result) {
+
+}
+
+int main(int argc, char const *argv[])
+{
+	char* filename = (char*)argv[1];
+	
+	maze_reader(filename);
+
+	/*
+		0: Empty
+		1: Wall
+		2: Dot
+		3: Goal
+	*/
+	return 0;
+}
